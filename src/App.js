1581e371b3856f111b6dd0a72f700655f92be1e7
Duc Nguyen <duc@carousellmotors.com>
2017-02-22 18:23:45 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { FormattedMessage, FormattedNumber } from 'react-intl';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Eric',
+            unreadCount: 1000,
+        };
+    }
+
+    render() {
+        const { name, unreadCount } = this.state;
+
+        return (
+            <div>
+                <div>
+                    normal:
+
+                    <div>
+                        <code>
+                            {`<FormattedNumber
+                            value={5000}
+                        />`}
+                        </code>
+                    </div>
+                    --------
+                    <div>
+                        <FormattedNumber
+                            value={5000}
+                        />
+                    </div>
+                </div>
+                <hr/>
+                <div>
+                    Currency
+                    <pre>
+                        Mac:
+                        - Chrome: OK
+                        - Firefox: Error
+
+                        iOS:
+                        - Chrome: error
+                        - Safari: OK
+
+                        Android:
+                        - Nexus 5: OK
+                    </pre>
+
+                    <div>
+                        <code>
+                            {`
+                    <FormattedNumber
+                        value={5000}
+                        style="currency"
+                    />`}
+                        </code>
+                    </div>
+
+
+                    <div>
+                        <FormattedNumber
+                            value={5000}
+                            style={'currency'}
+                            currency="USD"
+                            currencyDisplay="symbol"
+                            maximumFractionDigits={0}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
